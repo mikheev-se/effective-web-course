1076bdc3437d7a395816cd0d35653351a9bd556e
Sergey Mikheev <sergeypolosh@gmail.com>
2022-11-25 13:44:23 +0600

    refactor: move boilerplate code for page content to separate component

--- react-ts/src/components/Characters/Characters.tsx
+++ react-ts/src/components/Characters/Characters.tsx
@@ -1,45 +1,8 @@
-import { Button, TextField } from '@mui/material';
-import Card from '../Card/Card';
 import { characters } from '../../mocks';
+import Content from '../Content/Content';
 
 function Characters() {
-  sessionStorage['currentPage'] = 'characters';
-  return (
-    <main>
-      <div className='main__title'>
-        <h1>
-          Characters <span className='title__count'>({characters.length})</span>
-        </h1>
-      </div>
-      <form className='search-form'>
-        <TextField
-          type='search'
-          placeholder='Search for Characters by Name'
-          size='small'
-          sx={{
-            width: '100%',
-          }}
-        ></TextField>
-        <Button
-          variant='contained'
-          sx={{
-            backgroundColor: 'var(--search-yellow)',
-            width: '320px',
-            ':hover': {
-              backgroundColor: 'var(--search-yellow)',
-            },
-          }}
-        >
-          Search
-        </Button>
-      </form>
-      <div className='main__content'>
-        {characters.map((character) => (
-          <Card {...character} key={character.id} />
-        ))}
-      </div>
-    </main>
-  );
+  return <Content name='Characters' items={characters} />;
 }
 
 export default Characters;
--- react-ts/src/components/Comics/Comics.tsx
+++ react-ts/src/components/Comics/Comics.tsx
@@ -1,45 +1,8 @@
-import { Button, TextField } from '@mui/material';
 import { comics } from '../../mocks';
-import Card from '../Card/Card';
+import Content from '../Content/Content';
 
 function Comics() {
-  sessionStorage['currentPage'] = 'comics';
-  return (
-    <main>
-      <div className='main__title'>
-        <h1>
-          Comics <span className='title__count'>({comics.length})</span>
-        </h1>
-      </div>
-      <form className='search-form'>
-        <TextField
-          type='search'
-          placeholder='Search for Comics by Name'
-          size='small'
-          sx={{
-            width: '100%',
-          }}
-        ></TextField>
-        <Button
-          variant='contained'
-          sx={{
-            backgroundColor: 'var(--search-yellow)',
-            width: '320px',
-            ':hover': {
-              backgroundColor: 'var(--search-yellow)',
-            },
-          }}
-        >
-          Search
-        </Button>
-      </form>
-      <div className='main__content'>
-        {comics.map((comic) => (
-          <Card {...comic} key={comic.id} />
-        ))}
-      </div>
-    </main>
-  );
+  return <Content name='Comics' items={comics} />;
 }
 
 export default Comics;
